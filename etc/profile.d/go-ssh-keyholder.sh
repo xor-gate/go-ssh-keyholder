09e3ffb9f214316c77f1c4b8d1973a4a061a9d8e
Jerry Jacobs <xor-gate@users.noreply.github.com>
2023-07-16 12:15:59 +0200

    Update and rename go-keyholder.sh to go-ssh-keyholder.sh

--- etc/profile.d/go-ssh-keyholder.sh
+++ etc/profile.d/go-ssh-keyholder.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if [ -z "$SSH_AUTH_SOCK" ]; then
-  SSH_AUTH_SOCK=/var/run/go-keyholder.sock
+  SSH_AUTH_SOCK=/var/run/go-ssh-keyholder.sock
   readonly SSH_AUTH_SOCK
   export SSH_AUTH_SOCK
 fi
